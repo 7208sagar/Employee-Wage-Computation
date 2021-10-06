cb769de43fb03ebb634a03a7b2410a91c6513f7a
sagar <7208sk@gmail.com>
2021-10-06 16:30:45 +0530

    Emp present or ansent

--- Program.cs
+++ Program.cs
@@ -8,7 +8,19 @@ namespace EmployeeWage
         {
             
             Console.WriteLine("Welcome to Employee wage Computation");
+            
+
+            //UC1
+            int employee = 1;
+            Random random = new Random();
+            int check = random.Next(0,2);//0,1
+            if (check ==employee)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
             Console.ReadLine();
+
         }
+
     }
 }
