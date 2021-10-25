e7e9433042cb1d082b7a0f83894f364ea030bac4
sagar <7208sk@gmail.com>
2021-10-25 17:56:45 +0530

    Calculate total Wage for each Company

--- Program.cs
+++ Program.cs
@@ -6,9 +6,16 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            
             Console.WriteLine("Welcome to Employee wage Computation");
-            Console.ReadLine();
+            EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 2, 10);
+            EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
         }
     }
 }
+
+
+            
