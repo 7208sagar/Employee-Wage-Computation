26f089060ec275adc157575e59f0548ab33dd163
sagar <7208sk@gmail.com>
2021-10-25 17:25:37 +0530

    Calculate monthly wage

--- Program.cs
+++ Program.cs
@@ -4,24 +4,20 @@ namespace EmployeeWage
 {
     class Program
     {
-        //UC4
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome to Employee wage Computation");
-
+            Console.WriteLine("Welcome to employee wage computation program");
             int empHrs = 0;
             int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpWages = 0;
             for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -33,14 +29,12 @@ namespace EmployeeWage
                     default:
                         empHrs = 0;
                         break;
-
                 }
-
                 empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage = totalEmpWage + empWage;
-                Console.WriteLine("Employee wage is=" + empWage);
+                totalEmpWages = totalEmpWages + empWage;
+                Console.WriteLine("Employee wages is=" + empWage);
             }
-            Console.ReadLine();
+            Console.WriteLine("total employee wage is=" + totalEmpWages);
         }
     }
 }
